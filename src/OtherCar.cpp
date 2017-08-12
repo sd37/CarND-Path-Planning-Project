710b6dd4b876bac625547238413a1dfa7b480eee
spandan <spandan@prosperworks.com>
2017-08-11 21:41:52 -0700

    final_version

---
+++ src/OtherCar.cpp
@@ -0,0 +1,11 @@
+//
+// Created by spandan on 8/10/17.
+//
+
+#include "OtherCar.h"
+
+OtherCar::OtherCar(int id, double s, double d) {
+    this->id = id;
+    this->s = s;
+    this->d = d;
+}
---
+++ src/OtherCar.h
@@ -0,0 +1,18 @@
+//
+// Created by spandan on 8/10/17.
+//
+
+#ifndef PATH_PLANNING_OTHERCAR_H
+#define PATH_PLANNING_OTHERCAR_H
+
+class OtherCar {
+public:
+    int id;
+    double s;
+    double d;
+
+    OtherCar(int id, double s, double d);
+};
+
+
+#endif //PATH_PLANNING_OTHERCAR_H
